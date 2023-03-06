7113ba00f238ca2673967fdcdc1ac262adb1e9bb
John U <arjorb@gmail.com>
2023-03-06 11:46:18 +0200

    done fetching the data from the api

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,5 +1,17 @@
+import { useState, useEffect } from 'react';
 import Question from './Question';
 const Questions = () => {
+  const [question, setQuestion] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch('https://opentdb.com/api.php?amount=5');
+      const { results } = await res.json();
+      console.log(results);
+    };
+
+    getData();
+  }, []);
   return (
     <div>
       <Question />
