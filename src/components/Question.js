62687a77baf3873876aa36be36b08f35997781df
John U <arjorb@gmail.com>
2023-03-06 15:59:31 +0200

    install the nanoid to generate random ids

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,20 +1,37 @@
 import { useState } from 'react';
+import { nanoid } from 'nanoid';
 const Question = ({ question, correct_answer, incorrect_answers }) => {
-  const [response, setResponse] = useState(shuffledHandler());
+  const [response, setResponse] = useState([{ value: shuffledHandler() }]);
 
   function shuffledHandler() {
     const answers = [...[correct_answer], ...incorrect_answers];
     const shuffled = answers.sort(() => Math.random() - 0.5);
+    const newArray = shuffled.map(item => {
+      return {
+        id: nanoid(),
+        value: item,
+        isSelected: false,
+      };
+    });
     return shuffled;
   }
 
-  const responseElement = response.map(response => (
-    <div className='border-2 border-gray-200 px-5 rounded-full'>{response}</div>
+  function selectResponse(id) {
+    console.log(`cliked ${id}`);
+  }
+
+  const responseElement = response.map((response, index) => (
+    <div
+      className={`border-2 border-gray-200 px-5 rounded-full ${isSelected ? 'bg-green-400' : ''}`}
+      onClick={() => selectResponse(index)}
+    >
+      {response}
+    </div>
   ));
 
   return (
     <div className='mt-10 pb-3 border-b-2 w-9/12'>
-      <h1 className='text-2xl'>{question}</h1>
+      <h1 className='text-xl font-semibold' dangerouslySetInnerHTML={{ __html: question }} />
       <div className='flex gap-4 mt-2'>{responseElement}</div>
     </div>
   );
