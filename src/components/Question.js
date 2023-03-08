a17c1ea1ee7a09cd69e8aa56836d8d3a4f360b68
John U <arjorb@gmail.com>
2023-03-08 15:06:22 +0200

    done working on the refactoring of the main state and the function of shuffleHandler

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,15 @@
 import { useState } from 'react';
 
-const Question = props => {
-  // const responseElement = response.map(response => (
-  //   <div className={`border-2 border-gray-200 px-5 rounded-full cursor-pointer`}>{response.value}</div>
-  // ));
-
+const Question = ({ question, answers }) => {
+  const responseElement = answers.map((response, index) => (
+    <div key={index} className={`border-2 border-gray-200 px-5 rounded-full cursor-pointer`}>
+      {response}
+    </div>
+  ));
   return (
     <div className='mt-10 pb-3 border-b-2 w-9/12'>
-      {/* <h1 className='text-xl font-semibold' dangerouslySetInnerHTML={{ __html: props.data.question }} /> */}
-      {/* <div className='flex gap-4 mt-2'>{responseElement}</div> */}
-      <div>Placeholder</div>
+      <h1 className='text-xl font-semibold' dangerouslySetInnerHTML={{ __html: question }} />
+      <div className='flex gap-4 mt-2'>{responseElement}</div>
     </div>
   );
 };
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,35 +1,55 @@
 import { useState, useEffect } from 'react';
-// import { nanoid } from 'nanoid';
+import { nanoid } from 'nanoid';
 import Question from './Question';
+
 const Questions = () => {
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  // const [response, setResponse] = useState(shuffledHandler());
-  // const [currentSelect, setCurrentSelect] = useState();
-
-  // variables
+  const [loading, setLoading] = useState(false);
 
   // fetching the data from the api
-
   useEffect(() => {
+    setLoading(true);
     async function getData() {
       const res = await fetch('https://opentdb.com/api.php?amount=5');
       const { results } = await res.json();
-      setData(results);
+      const questions = results.map(question => ({
+        question: question.question,
+        correct: question.correct_answer,
+        incorrect: question.incorrect_answers,
+        answers: shuffledHandler(question.correct_answer, question.incorrect_answers),
+        id: nanoid(),
+        isSelected: false,
+      }));
+      setData(questions);
+      setLoading(false);
     }
     getData();
   }, []);
 
-  console.log(data);
-  const QuestionElement = data.map((question, index) => <Question key={index} {...data} />);
+  // Generate the random array for answers
+  function shuffledHandler(correct, incorrect) {
+    const answers = [...[correct], ...incorrect];
+    const shuffled = answers.sort(() => Math.random() - 0.5);
+    const newArray = shuffled.map(item => item);
+    return newArray;
+  }
+
+  const QuestionElement = data.map((question, index) => <Question key={index} {...question} />);
 
   return (
     <div>
-      <div>{QuestionElement}</div>
-      <div className='mt-10 bg-indigo-400 py-3 px-7 w-max rounded-md cursor-pointer text-white font-semibold'>
-        Check Answer
-      </div>
+      {loading ? (
+        <div className='flex justify-center'>
+          <img src='loader.svg' alt='loading...' />
+        </div>
+      ) : (
+        <div>
+          <div>{QuestionElement}</div>
+          <div className='mt-10 bg-indigo-400 py-3 px-7 w-max rounded-md cursor-pointer text-white font-semibold'>
+            Check Answer
+          </div>
+        </div>
+      )}
     </div>
   );
 };
