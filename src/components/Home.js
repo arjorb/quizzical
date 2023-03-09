26ee079f0ac8ded40e60f8263417153d5f8c865e
John U <arjorb@gmail.com>
2023-03-09 11:39:27 +0200

    fixing the ui for the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 const Home = ({ handleStart }) => {
   return (
-    <div className='flex flex-col justify-center items-center'>
-      <h1 className='text-2xl font-semibold '>Quizzical</h1>
+    <div className='flex flex-col justify-center items-center h-screen space-y-3'>
+      <h1 className='text-3xl font-semibold '>Quizzical</h1>
       <p>Some description if needed</p>
       <div className='bg-indigo-400 py-2 px-7 text-white cursor-pointer rounded-sm' onClick={handleStart}>
         Start quiz
