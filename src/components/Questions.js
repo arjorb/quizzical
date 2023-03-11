31aee6f3b7fb06f8dbb48e38fffd8d651fc09b31
John U <arjorb@gmail.com>
2023-03-11 09:56:31 +0200

    done refactoring the questions component

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -5,34 +5,37 @@ const Questions = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [checked, setChecked] = useState(false);
+  const [success, setSuccess] = useState(Array.from({ length: 5 }));
+
+  const getData = async () => {
+    setLoading(true);
+    const res = await fetch('https://opentdb.com/api.php?amount=5');
+    const { results } = await res.json();
+    const questions = results.map((question, index) => ({
+      question: question.question,
+      correct: question.correct_answer,
+      answers: shuffledHandler(question.correct_answer, question.incorrect_answers),
+      id: index,
+    }));
+    setData(questions);
+    setLoading(false);
+  };
 
   useEffect(() => {
-    setLoading(true);
-    async function getData() {
-      const res = await fetch('https://opentdb.com/api.php?amount=5');
-      const { results } = await res.json();
-      const questions = results.map((question, index) => ({
-        question: question.question,
-        correct: question.correct_answer,
-        incorrect: question.incorrect_answers,
-        answers: shuffledHandler(question.correct_answer, question.incorrect_answers),
-        id: index,
-      }));
-      setData(questions);
-      setLoading(false);
-    }
     getData();
   }, []);
 
-  // Generate the random array for answers
-  function shuffledHandler(correct, incorrect) {
+  const shuffledHandler = (correct, incorrect) => {
     const answers = [...[correct], ...incorrect];
     const shuffled = answers.sort(() => Math.random() - 0.5);
     const newArray = shuffled.map(item => item);
     return newArray;
-  }
+  };
 
-  const [success, setSuccess] = useState(Array.from({ length: 5 }));
+  const resetHandler = () => {
+    getData();
+    setChecked(false);
+  };
 
   const QuestionElement = data.map((question, index) => (
     <Question key={index} {...question} checked={checked} setSuccess={setSuccess} />
@@ -50,16 +53,11 @@ const Questions = () => {
           {checked ? (
             <div className='flex items-center gap-16 max-w-96 mx-auto mt-5 pl-40'>
               <h1 className='font-bold text-xl leading-4'>
-                <span>You scored </span>
-                <span>
-                  {success.filter(x => x).length}/{data.length}
-                </span>
-                <span> correct answers</span>
+                <p>
+                  You scored {success.filter(x => x).length}/{data.length} correct answers
+                </p>
               </h1>
-              <button
-                onClick={() => window.location.reload(false)}
-                className='text-white bg-indigo-500 rounded-md px-5 py-2'
-              >
+              <button onClick={resetHandler} className='text-white bg-indigo-500 rounded-md px-5 py-2'>
                 Play again
               </button>
             </div>
