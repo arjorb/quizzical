5771e834959168e21d952aca43bd2cb040a99844
John U <arjorb@gmail.com>
2023-03-12 17:43:59 +0200

    refactoring the hadnleStart funciton

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Home from './components/Home';
 
 const App = () => {
   const [start, setStart] = useState(false);
-  const handleStart = () => {
-    setStart(true);
-  };
+  const handleStart = () => setStart(true);
   return (
     <div className='bg-background min-h-screen bg-no-repeat bg-cover'>
       {!start ? <Home handleStart={handleStart} /> : <Questions />}
